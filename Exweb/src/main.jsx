a05ad610f47d4602a36da186b1a70749c3f4001d
Kiran Kishor <kirankarthika12@gmail.com>
2025-02-04 14:00:14 +0530

    Update main.jsx

--- Exweb/src/main.jsx
+++ Exweb/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Ex-media">
+    <BrowserRouter>
       <App />
     </BrowserRouter>  
   </StrictMode>,
